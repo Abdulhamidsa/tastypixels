82e333d14e7a5d18664a41e38d072ce0da35802a
Aboood <abdu3793@stud.kea.dk>
2024-03-11 11:55:59 +0100

    ee

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,4 +1,4 @@
-import { connectDB, fetchData } from "@/database/dataFetcher";
+import { connectDB, fetchData } from "../../database/dataFetcher";
 
 export default async function handler(req, res) {
   const userData = (await fetchData()) || [];
