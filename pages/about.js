cf8c1dd014154a15d3f4aba3b162628d9f70dbe4
Aboood <abdu3793@stud.kea.dk>
2024-04-04 12:52:43 +0200

    "..."

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,65 @@
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch("https://api.github.com/repos/vercel/next.js");
-  const repo = await res.json();
-  // Pass data to the page via props
-  return { props: { repo } };
+import { Box, Badge, Image, Text } from "@chakra-ui/react";
+
+export default function about({ photos }) {
+  console.log("Photos:", photos);
+  return (
+    <>
+      {" "}
+      <Box>
+        sss
+        {photos.map((photo) => (
+          <Box key={photo._id} m={4}>
+            <Image src={photo.imageUrl} alt={photo.title} maxW="sm" borderWidth="1px" borderRadius="lg" />
+
+            <Box p="6">
+              <Box d="flex" alignItems="baseline">
+                {photo.category.map((category, index) => (
+                  <Badge key={`${category}-${index}`} borderRadius="full" px="2" colorScheme="teal" mr="2">
+                    {category}
+                  </Badge>
+                ))}
+              </Box>
+
+              <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
+                {photo.title}
+              </Box>
+
+              <Text mt="2" color="gray.600">
+                {photo.description}
+              </Text>
+
+              <Box>
+                {photo.tags.map((tag, index) => (
+                  <Badge key={`${tag}-${index}`} borderRadius="full" px="2" colorScheme="blue" mt="2" mr="2">
+                    {tag}
+                  </Badge>
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
 }
 
-export default function Page({ repo }) {
-  return (
-    <main>
-      <p>{repo.stargazers_count}</p>
-    </main>
-  );
+export async function getServerSideProps() {
+  try {
+    const res = await fetch("/api/api-get-photos");
+    if (!res.ok) {
+      throw new Error("Failed to fetch photos");
+    }
+    const photos = await res.json();
+    return {
+      props: { photos },
+    };
+  } catch (error) {
+    console.error("Error fetching photos:", error);
+    return {
+      props: {
+        photos: [],
+        error: "Failed to fetch photos",
+      },
+    };
+  }
 }
--- pages/api/api-get-photos.js
+++ pages/api/api-get-photos.js
@@ -3,14 +3,13 @@ import dbConnect from "../../database/db";
 import ME from "../../models/Photo";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
+  //   if (req.method !== "GET") {
+  //     return res.status(405).json({ message: "Method Not Allowed" });
+  //   }
 
   try {
     await dbConnect();
     const photos = await ME.find({});
-
     return res.status(200).json(photos);
   } catch (error) {
     console.error("Error fetching photos:", error);
