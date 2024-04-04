18dfc7ebf15879a87acff207ea3f7706eadff4ef
Aboood <abdu3793@stud.kea.dk>
2024-04-04 12:14:13 +0200

    "..."

--- pages/about.js
+++ pages/about.js
@@ -1,52 +1,15 @@
-import { Box, Badge, Image, Text } from "@chakra-ui/react";
-
-export default function about({ photos }) {
-  console.log("Photos:", photos);
-  return (
-    <>
-      {" "}
-      <Box>
-        sss
-        {photos.map((photo) => (
-          <Box key={photo._id} m={4}>
-            <Image src={photo.imageUrl} alt={photo.title} maxW="sm" borderWidth="1px" borderRadius="lg" />
-
-            <Box p="6">
-              <Box d="flex" alignItems="baseline">
-                {photo.category.map((category, index) => (
-                  <Badge key={`${category}-${index}`} borderRadius="full" px="2" colorScheme="teal" mr="2">
-                    {category}
-                  </Badge>
-                ))}
-              </Box>
-
-              <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-                {photo.title}
-              </Box>
-
-              <Text mt="2" color="gray.600">
-                {photo.description}
-              </Text>
-
-              <Box>
-                {photo.tags.map((tag, index) => (
-                  <Badge key={`${tag}-${index}`} borderRadius="full" px="2" colorScheme="blue" mt="2" mr="2">
-                    {tag}
-                  </Badge>
-                ))}
-              </Box>
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    </>
-  );
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const repo = await res.json();
+  // Pass data to the page via props
+  return { props: { repo } };
 }
 
-export async function getServerSideProps() {
-  const res = await fetch("/api/api-get-photos");
-  const photos = await res.json();
-  return {
-    props: { photos },
-  };
+export default function Page({ repo }) {
+  return (
+    <main>
+      <p>{repo.stargazers_count}</p>
+    </main>
+  );
 }
