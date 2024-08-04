b16baf6e593fe1a9d747735767116c567627b616
Aboood <abdu3793@stud.kea.dk>
2024-08-05 00:45:23 +0200

    add missing module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("module-alias/register");
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./.env.local" });
 // require('dotenv').config();
 
