cc5c74f645e07b314fb551fb2a666375f9c6eeeb
Aboood <abdu3793@stud.kea.dk>
2024-08-05 00:12:15 +0200

    add domain to allow cros orgin

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://tastypixels-git-developing-abdulhamidsas-projects.vercel.app"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
