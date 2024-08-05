18837e5a57784478921d09374c5a32ba747dd5dd
Aboood <abdu3793@stud.kea.dk>
2024-08-05 19:41:49 +0200

    remove unused api from nextjs api

--- frontend/pages/api/api-fetch-recipe.js
+++ frontend/pages/api/api-fetch-recipe.js
@@ -1,47 +1,47 @@
-import connectToMongoDB from "@/backend/database/db";
-export default async function handler(req, res) {
-  try {
-    const db = await connectToMongoDB();
-    const usersData = await db.collection("members").find({}).toArray();
-    const enhancedUploads = [];
-    for (const user of usersData) {
-      const hasLikedPosts = user.likedPosts && Array.isArray(user.likedPosts);
-      const hasDislikedPosts = user.dislikedPosts && Array.isArray(user.dislikedPosts);
-      if (hasLikedPosts || hasDislikedPosts) {
-        for (const upload of user.uploads) {
-          let liked = false;
-          let disliked = false;
-          if (hasLikedPosts) {
-            liked = user.likedPosts.some((post) => post.uploadId.toString() === upload._id.toString());
-          }
-          if (hasDislikedPosts) {
-            disliked = user.dislikedPosts.some((post) => post.uploadId.toString() === upload._id.toString());
-          }
-          enhancedUploads.push({
-            ...upload,
-            _id: upload._id.toString(),
-            userId: user._id.toString(),
-            username: user.username,
-            liked,
-            disliked,
-          });
-        }
-      } else {
-        for (const upload of user.uploads) {
-          enhancedUploads.push({
-            ...upload,
-            _id: upload._id.toString(),
-            userId: user._id.toString(),
-            username: user.username,
-            liked: false,
-            disliked: false,
-          });
-        }
-      }
-    }
-    res.status(200).json(enhancedUploads);
-  } catch (error) {
-    console.error("Error fetching uploads:", error);
-    res.status(500).json({ error: "Failed to fetch uploads" });
-  }
-}
+// import connectToMongoDB from "@/backend/database/db";
+// export default async function handler(req, res) {
+//   try {
+//     const db = await connectToMongoDB();
+//     const usersData = await db.collection("members").find({}).toArray();
+//     const enhancedUploads = [];
+//     for (const user of usersData) {
+//       const hasLikedPosts = user.likedPosts && Array.isArray(user.likedPosts);
+//       const hasDislikedPosts = user.dislikedPosts && Array.isArray(user.dislikedPosts);
+//       if (hasLikedPosts || hasDislikedPosts) {
+//         for (const upload of user.uploads) {
+//           let liked = false;
+//           let disliked = false;
+//           if (hasLikedPosts) {
+//             liked = user.likedPosts.some((post) => post.uploadId.toString() === upload._id.toString());
+//           }
+//           if (hasDislikedPosts) {
+//             disliked = user.dislikedPosts.some((post) => post.uploadId.toString() === upload._id.toString());
+//           }
+//           enhancedUploads.push({
+//             ...upload,
+//             _id: upload._id.toString(),
+//             userId: user._id.toString(),
+//             username: user.username,
+//             liked,
+//             disliked,
+//           });
+//         }
+//       } else {
+//         for (const upload of user.uploads) {
+//           enhancedUploads.push({
+//             ...upload,
+//             _id: upload._id.toString(),
+//             userId: user._id.toString(),
+//             username: user.username,
+//             liked: false,
+//             disliked: false,
+//           });
+//         }
+//       }
+//     }
+//     res.status(200).json(enhancedUploads);
+//   } catch (error) {
+//     console.error("Error fetching uploads:", error);
+//     res.status(500).json({ error: "Failed to fetch uploads" });
+//   }
+// }
--- frontend/pages/api/api-upload-img.js
+++ frontend/pages/api/api-upload-img.js
@@ -1,43 +1,43 @@
-import userDemo from "@/backend/models/User";
-import connectToMongoDB from "@/backend/database/db";
-import mongoose from "mongoose";
+// import userDemo from "@/backend/models/User";
+// import connectToMongoDB from "@/backend/database/db";
+// import mongoose from "mongoose";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Method Not Allowed" });
+//   }
 
-  const { imageUrl, title, description, category, tags, userId } = req.body;
-  // console.log("Received userId:", userId);
+//   const { imageUrl, title, description, category, tags, userId } = req.body;
+//   // console.log("Received userId:", userId);
 
-  try {
-    await connectToMongoDB();
-    const userIdObj = new mongoose.Types.ObjectId(userId);
-    let user = await userDemo.findById(userIdObj);
+//   try {
+//     await connectToMongoDB();
+//     const userIdObj = new mongoose.Types.ObjectId(userId);
+//     let user = await userDemo.findById(userIdObj);
 
-    if (!user) {
-      return res.status(404).json({ errors: ["User not found"] });
-    }
+//     if (!user) {
+//       return res.status(404).json({ errors: ["User not found"] });
+//     }
 
-    console.log("Current number of uploads:", user.uploads.length);
+//     console.log("Current number of uploads:", user.uploads.length);
 
-    if (user.uploads.length >= 4) {
-      return res.status(400).json({ errors: ["Uploads Limit reached! Remove some uploads from your profile to add more."] });
-    }
-    const newData = {
-      _id: new mongoose.Types.ObjectId(),
-      imageUrl,
-      title,
-      description,
-      category,
-      tags,
-    };
-    user.uploads.push(newData);
-    await user.save();
+//     if (user.uploads.length >= 4) {
+//       return res.status(400).json({ errors: ["Uploads Limit reached! Remove some uploads from your profile to add more."] });
+//     }
+//     const newData = {
+//       _id: new mongoose.Types.ObjectId(),
+//       imageUrl,
+//       title,
+//       description,
+//       category,
+//       tags,
+//     };
+//     user.uploads.push(newData);
+//     await user.save();
 
-    return res.status(200).json({ message: "Upload saved successfully" });
-  } catch (error) {
-    console.error("Error saving photo to database:", error);
-    return res.status(500).json({ errors: ["Server error"] });
-  }
-}
+//     return res.status(200).json({ message: "Upload saved successfully" });
+//   } catch (error) {
+//     console.error("Error saving photo to database:", error);
+//     return res.status(500).json({ errors: ["Server error"] });
+//   }
+// }
