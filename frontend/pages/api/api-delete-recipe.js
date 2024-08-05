b80900978c91e2d6519a04bace6092f1c179ec77
Aboood <abdu3793@stud.kea.dk>
2024-08-05 19:58:27 +0200

    edit api

--- frontend/pages/api/api-delete-recipe.js
+++ frontend/pages/api/api-delete-recipe.js
@@ -1,31 +1,31 @@
-import User from "@/backend/models/User";
-import connectToMongoDB from "@/backend/database/db";
-import mongoose from "mongoose";
+// import User from "@/backend/models/User";
+// import connectToMongoDB from "@/backend/database/db";
+// import mongoose from "mongoose";
 
-export default async function handler(req, res) {
-  if (req.method !== "DELETE") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "DELETE") {
+//     return res.status(405).json({ message: "Method Not Allowed" });
+//   }
 
-  const { userId, Id } = req.body;
-  console.log(userId, Id);
+//   const { userId, Id } = req.body;
+//   console.log(userId, Id);
 
-  try {
-    await connectToMongoDB();
-    const userIdObj = new mongoose.Types.ObjectId(userId);
-    let user = await User.findById(userIdObj);
+//   try {
+//     await connectToMongoDB();
+//     const userIdObj = new mongoose.Types.ObjectId(userId);
+//     let user = await User.findById(userIdObj);
 
-    if (!user) {
-      return res.status(404).json({ errors: ["User not found"] });
-    }
+//     if (!user) {
+//       return res.status(404).json({ errors: ["User not found"] });
+//     }
 
-    user.uploads = user.uploads.filter((upload) => upload._id.toString() !== Id);
+//     user.uploads = user.uploads.filter((upload) => upload._id.toString() !== Id);
 
-    await user.save();
+//     await user.save();
 
-    return res.status(200).json({ message: "Upload deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting upload:", error);
-    return res.status(500).json({ errors: ["Server error"] });
-  }
-}
+//     return res.status(200).json({ message: "Upload deleted successfully" });
+//   } catch (error) {
+//     console.error("Error deleting upload:", error);
+//     return res.status(500).json({ errors: ["Server error"] });
+//   }
+// }
