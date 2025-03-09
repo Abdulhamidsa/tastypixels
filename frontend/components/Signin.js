85f310a94bc35bb2a6a11098160c91efd38523d0
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-03-09 12:04:14 +0100

    refactor Signin and Signup components for improved layout, styling, and user experience

--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -1,6 +1,7 @@
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
-import { FormControl, Link as ChakraLink, Text, FormLabel, Input, FormErrorMessage, Button, useToast, Box } from "@chakra-ui/react";
+import { FormControl, Text, FormLabel, Input, FormErrorMessage, Button, useToast, Box, VStack, Heading, Divider, Flex, Icon, Container } from "@chakra-ui/react";
+import { FiLogIn } from "react-icons/fi";
 import { useAuth } from "@/context/AuthContext";
 
 const validationSchema = Yup.object({
@@ -36,50 +37,70 @@ const Signin = ({ onModalOpen, onModalClose, setFormType }) => {
   };
 
   return (
-    <Formik
-      initialValues={{ email: "a@a.com", password: "Aboood166" }}
-      validationSchema={validationSchema}
-      validateOnBlur={true}
-      validateOnChange={false}
-      onSubmit={async (values, { setSubmitting, setErrors }) => {
-        await handleLogin(values, setErrors);
-        setSubmitting(false);
-      }}
-    >
-      {({ errors }) => (
-        <Form>
-          <Field name="email">
-            {({ field, form }) => (
-              <FormControl isInvalid={errors.email && form.touched.email}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input {...field} id="email" placeholder="email" />
-                <FormErrorMessage>{errors.email}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
-          <Field name="password">
-            {({ field, form }) => (
-              <FormControl isInvalid={errors.password && form.touched.password}>
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input {...field} id="password" placeholder="password" type="password" autoComplete="true" />
-                <FormErrorMessage>{errors.password}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
-          {errors.loginError && (
-            <Box color="red.300" mt={2}>
-              {errors.loginError}
-            </Box>
-          )}
-          <Button mt={4} type="submit">
-            Sign In
-          </Button>
-          <Text as="span" onClick={handleSignUpClick} color="blue.500" mt={2} display="block" cursor="pointer">
-            Don't have an account yet? Sign up
+    <Flex align="center" justify="center" bg="transparent">
+      <Container maxW="lg" bg="transparent" p={{ base: 6, md: 8 }} textAlign="center">
+        <VStack spacing={4} align="center">
+          <Icon as={FiLogIn} boxSize={10} color="primary.500" />
+          <Heading fontSize={{ base: "2xl", md: "3xl" }} color="primary.700">
+            Welcome Back!
+          </Heading>
+          <Text fontSize="md" color="gray.600">
+            Sign in to continue
           </Text>
-        </Form>
-      )}
-    </Formik>
+        </VStack>
+
+        <Divider my={6} />
+
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          validateOnBlur={true}
+          validateOnChange={false}
+          onSubmit={async (values, { setSubmitting, setErrors }) => {
+            await handleLogin(values, setErrors);
+            setSubmitting(false);
+          }}
+        >
+          {({ errors, isSubmitting }) => (
+            <Form>
+              <Field name="email">
+                {({ field, form }) => (
+                  <FormControl isInvalid={errors.email && form.touched.email} mb={4}>
+                    <FormLabel htmlFor="email" color="secondary.700">
+                      Email Address
+                    </FormLabel>
+                    <Input {...field} id="email" placeholder="Enter your email" borderColor="gray.300" _focus={{ borderColor: "primary.500", boxShadow: "0 0 0 2px primary.100" }} size="lg" borderRadius="md" w="100%" />
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+
+              <Field name="password">
+                {({ field, form }) => (
+                  <FormControl isInvalid={errors.password && form.touched.password} mb={4}>
+                    <FormLabel htmlFor="password" color="secondary.700">
+                      Password
+                    </FormLabel>
+                    <Input {...field} id="password" placeholder="Enter your password" type="password" borderColor="gray.300" _focus={{ borderColor: "primary.500", boxShadow: "0 0 0 2px primary.100" }} size="lg" borderRadius="md" w="100%" />
+                    <FormErrorMessage>{errors.password}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+
+              {errors.loginError && (
+                <Box color="red.500" mt={2} textAlign="center">
+                  {errors.loginError}
+                </Box>
+              )}
+
+              <Button mt={6} w="100%" bg="primary.500" color="white" _hover={{ bg: "primary.600" }} _active={{ bg: "primary.700" }} size="lg" type="submit" isLoading={isSubmitting}>
+                Sign In
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </Flex>
   );
 };
 
--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import { signupValidationSchema } from "@/utils/validationSchemas";
-import { FormControl, Tooltip, FormHelperText, Text, FormLabel, Input, FormErrorMessage, Button, Box, useToast } from "@chakra-ui/react";
+import { FormControl, Tooltip, Text, FormLabel, Input, FormErrorMessage, Button, Box, useToast, VStack, Heading, Divider, Flex, Icon, Container } from "@chakra-ui/react";
 import { useAuth } from "@/context/AuthContext";
 import { UsernameField, EmailField } from "@/components/FormFields";
+import { FiUserPlus } from "react-icons/fi";
 
 const Signup = ({ onModalOpen, setFormType }) => {
   const {
@@ -19,16 +20,24 @@ const Signup = ({ onModalOpen, setFormType }) => {
   };
 
   const handleKeyUp = (e) => {
-    if (e.getModifierState("CapsLock")) {
-      setIsCapsLockActive(true);
-    } else {
-      setIsCapsLockActive(false);
-    }
+    setIsCapsLockActive(e.getModifierState("CapsLock"));
   };
 
   return (
-    <Box display="flex" flexDirection="row">
-      <Box width="100%" p={0}>
+    <Flex align="center" justify="center" bg="transparent">
+      <Container maxW="lg" bg="white" p={{ base: 6, md: 8 }} textAlign="center">
+        <VStack spacing={4} align="center">
+          <Icon as={FiUserPlus} boxSize={10} color="primary.500" />
+          <Heading fontSize={{ base: "2xl", md: "3xl" }} color="primary.700">
+            Create an Account
+          </Heading>
+          <Text fontSize="md" color="gray.600">
+            Join the Food Community
+          </Text>
+        </VStack>
+
+        <Divider my={6} />
+
         <Formik
           initialValues={{ username: "", email: "", password: "", repeatPassword: "" }}
           validationSchema={signupValidationSchema}
@@ -42,7 +51,7 @@ const Signup = ({ onModalOpen, setFormType }) => {
             try {
               await signup(values);
               toast({
-                title: "Signup Successful! Redirecting to signin form",
+                title: "Signup Successful! Redirecting...",
                 status: "success",
                 duration: 2000,
                 isClosable: true,
@@ -74,43 +83,61 @@ const Signup = ({ onModalOpen, setFormType }) => {
             <Form>
               <UsernameField />
               <EmailField />
+
               <Field name="password">
                 {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.password && form.touched.password}>
-                    <FormLabel htmlFor="password" color="white">
+                  <FormControl isInvalid={form.errors.password && form.touched.password} mb={4}>
+                    <FormLabel htmlFor="password" color="secondary.700">
                       Password
                     </FormLabel>
                     <Tooltip label="Caps Lock is on" isOpen={isCapsLockActive} placement="left">
-                      <Input {...field} id="password" placeholder="Password" type="password" onKeyUp={handleKeyUp} autoComplete="true" />
+                      <Input {...field} id="password" placeholder="Enter your password" type="password" onKeyUp={handleKeyUp} autoComplete="true" borderColor="gray.300" _focus={{ borderColor: "primary.500", boxShadow: "0 0 0 2px primary.100" }} size="lg" borderRadius="md" />
                     </Tooltip>
                     <FormErrorMessage>{form.errors.password}</FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
+
               <Field name="repeatPassword">
                 {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.repeatPassword && form.touched.repeatPassword}>
-                    <FormLabel htmlFor="repeatPassword" color="white">
-                      Repeat Password
+                  <FormControl isInvalid={form.errors.repeatPassword && form.touched.repeatPassword} mb={6}>
+                    <FormLabel htmlFor="repeatPassword" color="secondary.700">
+                      Confirm Password
                     </FormLabel>
                     <Tooltip label="Caps Lock is on" isOpen={isCapsLockActive} placement="left">
-                      <Input {...field} id="repeatPassword" placeholder="Repeat Password" type="password" onKeyUp={handleKeyUp} autoComplete="true" />
+                      <Input
+                        {...field}
+                        id="repeatPassword"
+                        placeholder="Repeat your password"
+                        type="password"
+                        onKeyUp={handleKeyUp}
+                        autoComplete="true"
+                        borderColor="gray.300"
+                        _focus={{ borderColor: "primary.500", boxShadow: "0 0 0 2px primary.100" }}
+                        size="lg"
+                        borderRadius="md"
+                      />
                     </Tooltip>
                     <FormErrorMessage>{form.errors.repeatPassword}</FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
-              <Button mt={4} colorScheme="yellow" isLoading={isSubmitting || loading} type="submit" disabled={!isValid || isSubmitting || loading}>
+
+              <Button mt={4} w="100%" bg="primary.500" color="white" _hover={{ bg: "primary.600" }} _active={{ bg: "primary.700" }} size="lg" type="submit" isLoading={isSubmitting || loading} disabled={!isValid || isSubmitting || loading}>
                 Sign Up
               </Button>
+
+              <Text mt={4} fontSize="sm">
+                Already have an account?{" "}
+                <Text as="span" color="primary.500" fontWeight="bold" cursor="pointer" onClick={handleSignInClick}>
+                  Sign in here
+                </Text>
+              </Text>
             </Form>
           )}
         </Formik>
-        <Text as="span" onClick={handleSignInClick} color="blue.500" mt={2} display="block" cursor="pointer">
-          Have an account already? Sign in here
-        </Text>
-      </Box>
-    </Box>
+      </Container>
+    </Flex>
   );
 };
 
