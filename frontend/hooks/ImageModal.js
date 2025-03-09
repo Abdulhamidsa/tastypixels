812efc8f85c51b5bd883d2715e74bc60f40d70c3
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-03-09 12:03:57 +0100

    refactor ImageModal component for improved layout and responsiveness

--- frontend/hooks/ImageModal.js
+++ frontend/hooks/ImageModal.js
@@ -4,23 +4,21 @@ import Image from "next/legacy/image";
 
 const ImageModal = ({ isOpen, onClose, selectedImage }) => {
   return (
-    <>
-      <Modal isOpen={isOpen} onClose={onClose} size="full">
-        <ModalOverlay />
-        <ModalContent bg="transparent" boxShadow="none" display="flex" alignItems="center" justifyContent="center">
-          <Box position="relative" w="100%" h="100%" display="flex" justifyContent="center" alignItems="center">
-            <CloseButton zIndex="1" onClick={onClose} size="lg" color="white" position="absolute" top="10px" right="10px" />
-            <TransformWrapper>
-              <TransformComponent>
-                <Box position="relative" maxW="650px" h="auto" display="flex" justifyContent="center" alignContent="center">
-                  <Image src={selectedImage} alt="Zoomed dish" objectFit="contain" layout="fixed" width={350} height={400} sizes="(max-width: 900px) 100vw, 600px" />
-                </Box>
-              </TransformComponent>
-            </TransformWrapper>
-          </Box>
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal isOpen={isOpen} onClose={onClose} size="full">
+      <ModalOverlay />
+      <ModalContent bg="transparent" boxShadow="none" display="flex" alignItems="center" justifyContent="center">
+        <Box position="relative" w="full" h="100%" display="flex" justifyContent="center" alignItems="center">
+          <CloseButton zIndex="1" onClick={onClose} size="lg" color="white" position="absolute" top="10px" right="10px" />
+          <TransformWrapper>
+            <TransformComponent>
+              <Box position="relative" maxWidth="90vw" maxHeight="90vh" width="100%" height="auto" display="flex" justifyContent="center" alignItems="center">
+                <Image src={selectedImage} alt="Zoomed dish" layout="intrinsic" objectFit="contain" width={800} height={500} />
+              </Box>
+            </TransformComponent>
+          </TransformWrapper>
+        </Box>
+      </ModalContent>
+    </Modal>
   );
 };
 
