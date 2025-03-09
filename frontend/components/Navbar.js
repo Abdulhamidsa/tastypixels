9aafdc0f33d8684027f0731074ed3f8a146cdc1f
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-03-09 13:17:04 +0100

    refactor Navbar component for improved user interaction and layout consistency

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Flex, IconButton, Box, Button, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack, Modal, ModalOverlay, ModalContent, ModalBody, Tab, Tabs, TabList, TabPanels, TabPanel, CloseButton } from "@chakra-ui/react";
+import { Flex, IconButton, Box, Button, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack, Modal, ModalContent, ModalBody, Tab, Tabs, TabList, TabPanels, TabPanel, CloseButton } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { FiLogOut } from "react-icons/fi";
 import SignUp from "@/components/Signup";
@@ -21,6 +21,7 @@ const Navbar = () => {
 
   const handleLogout = () => {
     logout();
+    onMenuClose();
     router.push("/");
   };
 
@@ -36,46 +37,6 @@ const Navbar = () => {
         {/* Mobile Menu Button */}
         <IconButton aria-label="Open menu" icon={<HamburgerIcon />} size="md" color="white" variant="outline" onClick={onMenuOpen} display={{ base: "block", md: "none" }} mr={4} />
 
-        {/* Desktop Links */}
-        <Flex align="center" display={{ base: "none", md: "flex" }} pr={3} gap={6}>
-          {isAuthenticated ? (
-            <>
-              <Button as={NextLink} href="/home" bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md">
-                Food Gallery
-              </Button>
-              {isSpecificPage && (
-                <>
-                  <Upload isOpen={isUploadOpen} closeMenu={onMenuClose} onClose={() => setIsUploadOpen(false)} />
-
-                  <Button bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={() => setIsUploadOpen(true)}>
-                    Upload
-                  </Button>
-                </>
-              )}
-              <Button as={NextLink} href={`/profile/${userName}`} bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md">
-                Profile
-              </Button>
-              <IconButton
-                aria-label="Logout"
-                icon={<FiLogOut />}
-                onClick={handleLogout}
-                variant="outline"
-                color="red.500"
-                border="1px"
-                _hover={{
-                  bg: "white",
-                  color: "red.600",
-                  transition: "0.2s ease-in-out",
-                }}
-              />
-            </>
-          ) : (
-            <Button bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={onModalOpen}>
-              Sign Up
-            </Button>
-          )}
-        </Flex>
-
         {/* Mobile Drawer */}
         <Drawer isOpen={isMenuOpen} placement="right" onClose={onMenuClose} size={{ base: "full", md: "sm" }}>
           <DrawerOverlay />
@@ -86,26 +47,46 @@ const Navbar = () => {
               <VStack spacing="20px">
                 {isAuthenticated ? (
                   <>
-                    <Button as={NextLink} href="/home" bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md">
+                    <Button as={NextLink} href="/home" bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={onMenuClose}>
                       Food Gallery
                     </Button>
-                    <Button as={NextLink} href={`/profile/${userName}`} bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md">
+                    <Button as={NextLink} href={`/profile/${userName}`} bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={onMenuClose}>
                       Profile
                     </Button>
-                    <IconButton aria-label="Logout" icon={<FiLogOut />} onClick={handleLogout} variant="outline" color="red.500" />
+                    {isSpecificPage && isAuthenticated && (
+                      <>
+                        <Upload isOpen={isUploadOpen} closeMenu={onMenuClose} onClose={() => setIsUploadOpen(false)} />
+                        <Button
+                          bg="transparent"
+                          border="1px"
+                          borderColor="primary.700"
+                          colorScheme="primary"
+                          size="md"
+                          onClick={() => {
+                            setIsUploadOpen(true);
+                            onMenuClose();
+                          }}
+                        >
+                          Upload
+                        </Button>
+                      </>
+                    )}
+                    <IconButton aria-label="Logout" icon={<FiLogOut />} onClick={handleLogout} variant="outline" color="white" />
                   </>
                 ) : (
-                  <Button bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={onModalOpen}>
+                  <Button
+                    bg="transparent"
+                    border="1px"
+                    borderColor="primary.700"
+                    colorScheme="primary"
+                    size="md"
+                    onClick={() => {
+                      onModalOpen();
+                      onMenuClose();
+                    }}
+                  >
                     Sign Up
                   </Button>
-                )}
-                {isSpecificPage && isAuthenticated && (
-                  <>
-                    <Upload isOpen={isUploadOpen} closeMenu={onMenuClose} onClose={() => setIsUploadOpen(false)} />
-                    <Button bg="transparent" border="1px" borderColor="primary.700" colorScheme="primary" size="md" onClick={() => setIsUploadOpen(true)}>
-                      Upload
-                    </Button>
-                  </>
                 )}
               </VStack>
             </DrawerBody>
@@ -115,7 +96,7 @@ const Navbar = () => {
 
       {/* Auth Modal with Tabs */}
       <Modal isOpen={isModalOpen} onClose={onModalClose}>
-        <ModalContent maxH="auto" overflowY="" maxW={["90vw", "70vw", "50vw", "40vw"]} borderRadius="lg" boxShadow="xl" bg="white" p="6" box Shadow="none">
+        <ModalContent maxH="auto" overflowY="" maxW={["90vw", "70vw", "50vw", "40vw"]} borderRadius="lg" boxShadow="xl" bg="white" p="6">
           <CloseButton size="xl" onClick={onModalClose} position="absolute" right="-35px" top="-15px" />
           <ModalBody p={0}>
             <Tabs mt="6" variant="soft-rounded" colorScheme="primary" isFitted>
