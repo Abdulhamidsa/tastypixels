28ee77e1d3e090f17602016442d54f9abce766de
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-03-13 13:50:14 +0100

    update CORS configuration to change localhost origin from port 3001 to 3000

--- backend/middlewares/corsConfig.js
+++ backend/middlewares/corsConfig.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:3001", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"],
+  origin: ["http://localhost:3000", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
