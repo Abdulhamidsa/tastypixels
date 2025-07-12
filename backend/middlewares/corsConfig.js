62935324b8c1373702373b4c9ee39210f394d006
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-07-13 00:34:09 +0200

    refactor: update CORS configuration to use a dynamic origin check for improved security

--- backend/middlewares/corsConfig.js
+++ backend/middlewares/corsConfig.js
@@ -1,7 +1,18 @@
 const cors = require("cors");
+
+const allowedOrigins = ["http://localhost:3000", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://api.norpus.com", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"];
+
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://api.norpus.com", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"],
-  optionsSuccessStatus: 200,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.warn("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
+  optionsSuccessStatus: 200,
 };
+
 module.exports = cors(corsOptions);
