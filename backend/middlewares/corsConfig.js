e742bcb2a6141db2fb8df219fce57b5c2223f0fe
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-07-13 10:41:53 +0200

    refactor: simplify CORS options by consolidating configuration into a single object

--- backend/middlewares/corsConfig.js
+++ backend/middlewares/corsConfig.js
@@ -2,18 +2,17 @@ const cors = require("cors");
 
 const allowedOrigins = ["http://localhost:3000", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://api.norpus.com", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"];
 
-const corsOptionsDelegate = function (req, callback) {
-  const origin = req.header("Origin");
-  if (!origin || allowedOrigins.includes(origin)) {
-    callback(null, {
-      origin: true,
-      credentials: true,
-      optionsSuccessStatus: 200,
-    });
-  } else {
-    console.warn("Blocked by CORS:", origin);
-    callback(new Error("Not allowed by CORS"));
-  }
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.warn("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
-module.exports = cors(corsOptionsDelegate);
+module.exports = cors(corsOptions);
