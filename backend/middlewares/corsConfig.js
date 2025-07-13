636d637b718532a29ed13cb16e8ee45e3c883d9d
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-07-13 09:35:14 +0200

    refactor: replace corsOptions with corsOptionsDelegate for improved origin handling

--- backend/middlewares/corsConfig.js
+++ backend/middlewares/corsConfig.js
@@ -2,17 +2,18 @@ const cors = require("cors");
 
 const allowedOrigins = ["http://localhost:3000", "https://tastypixels.vercel.app", "https://tastypixels.up.railway.app", "https://api.norpus.com", "https://tastypixels-vnvi-git-improvments-abdulhamidsas-projects.vercel.app"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.warn("Blocked by CORS:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
+const corsOptionsDelegate = function (req, callback) {
+  const origin = req.header("Origin");
+  if (!origin || allowedOrigins.includes(origin)) {
+    callback(null, {
+      origin: true,
+      credentials: true,
+      optionsSuccessStatus: 200,
+    });
+  } else {
+    console.warn("Blocked by CORS:", origin);
+    callback(new Error("Not allowed by CORS"));
+  }
 };
 
-module.exports = cors(corsOptions);
+module.exports = cors(corsOptionsDelegate);
